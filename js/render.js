a5937616bfbb56275ac4a1a6f91838af5c44ab3f
Visualizeit <vnyeit@gmail.com>
2025-06-11 18:23:24 +0800

    Update index.html and render.js to bind dynamic data to statistics display

--- js/render.js
+++ js/render.js
@@ -3,7 +3,21 @@
  * @param {typeof mockData} data - The data used to render the chart, should match the structure of mockData
  */
 const renderChart = ({ data }) => {
-  $('#order_count').text(data.order_count);
+  // 加盟商统计
+
+  // 订单统计
+  $("#order_count").text(data.order_count);
+  $("#today_order").text(data.today_order);
+  $("#yesterday_order").text(data.yesterday_order);
+  $("#month_order").text(data.month_order);
+
+  // 设备使用统计
+  $("#device_use_count").text(data.device_use_count);
+  $("#today_use_count").text(data.today_use_count);
+  $("#yesterday_use_count").text(data.yesterday_use_count);
+
+  // 会员统计
+  $("#member_count").text(data.member_count);
 };
 
 const initializeCharts = () => {
